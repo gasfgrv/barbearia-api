7a39ae42b6d64ab5cb02d83ad5f9ffdb3e9c30f5
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-04-27 14:38:19 -0300

    SecurityRequirement para requisições via SwaggerUI

--- src/main/kotlin/br/com/gusta/barbearia/agendamento/AgendamentoController.kt
+++ src/main/kotlin/br/com/gusta/barbearia/agendamento/AgendamentoController.kt
@@ -1,5 +1,6 @@
 package br.com.gusta.barbearia.agendamento
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import java.util.UUID
@@ -20,12 +21,14 @@ import org.springframework.web.bind.annotation.RestController
 class AgendamentoController @Autowired constructor(private val agendamentoService: AgendamentoService) {
 
     @GetMapping("/{id}")
+    @SecurityRequirement(name = "bearerAuth")
     fun consultarAgendamento(@PathVariable id: UUID?): Any {
         val agendamento = agendamentoService.consultarAgendamento(id!!)
         return ResponseEntity.ok().body(agendamento)
     }
 
     @PostMapping("/novo")
+    @SecurityRequirement(name = "bearerAuth")
     fun novoAgendamento(@RequestBody @Valid form: NovoAgendamentoForm): ResponseEntity<NovoAgendamentoResponse> {
         if (agendamentoService.existeAgendamentoNoMesmoHorario(form.horario, form.barbeiro)) {
             throw RuntimeException("Já existe agendamento para esse horário!")
