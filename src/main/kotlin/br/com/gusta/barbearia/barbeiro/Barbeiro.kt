e4e90d1a7191efb3d62aa79c129557eded4c5847
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-07 20:13:55 -0300

    Mudanças na feature de Barbeiro
    - Adição do relacionamento OneToMany com Agendamento e indentação da classe;
    - Verificação se barbeiro vai estar ocupado no horário solicitado.

--- src/main/kotlin/br/com/gusta/barbearia/barbeiro/Barbeiro.kt
+++ src/main/kotlin/br/com/gusta/barbearia/barbeiro/Barbeiro.kt
@@ -1,21 +1,27 @@
 package br.com.gusta.barbearia.barbeiro
 
+import br.com.gusta.barbearia.agendamento.Agendamento
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
 @Table(name = "barbeiro")
 data class Barbeiro(
-        @Id
-        @Column(name = "id")
-        @GeneratedValue(strategy = GenerationType.UUID)
-        var id: UUID? = null,
-        val nome: String,
-        val login: String,
-        val telefone: String
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UUID? = null,
+    val nome: String,
+    val login: String,
+    val telefone: String,
+    @OneToMany(mappedBy = "barbeiro", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    val agendamentos: List<Agendamento>
 )
--- src/main/kotlin/br/com/gusta/barbearia/barbeiro/BarbeiroService.kt
+++ src/main/kotlin/br/com/gusta/barbearia/barbeiro/BarbeiroService.kt
@@ -1,14 +1,89 @@
 package br.com.gusta.barbearia.barbeiro
 
+import br.com.gusta.barbearia.agendamento.Agendamento
+import br.com.gusta.barbearia.servico.Servico
+import java.time.LocalDateTime
 import java.util.UUID
 import org.springframework.stereotype.Service
 
 @Service
-class BarbeiroService(private val barbeiroRepository: BarbeiroRepository) {
+class BarbeiroService(
+    private val barbeiroRepository: BarbeiroRepository
+) {
 
     fun buscarBarbeiro(id: UUID): Barbeiro {
         return barbeiroRepository.findById(id)
-                .orElseThrow { RuntimeException() }
+            .orElseThrow { RuntimeException() }
+    }
+
+    fun verificaSeBarbeiroVaiEstarOcupadoNoHorario(horarioInicio: LocalDateTime, barbeiroId: UUID): Boolean {
+        val barbeiro = buscarBarbeiro(barbeiroId)
+        val agendamentosDoDia = filtraAgendamentosPorData(barbeiro, horarioInicio)
+        val horariosDoBarbeiro = getHorariosDoBarbeiro(
+            agendamentosDoDia,
+            getHorarioDeInicioDeCadaAgendamento(agendamentosDoDia),
+            getHorarioDeTerminoDeCadaAgendamento(agendamentosDoDia)
+        )
+        return verificaSeHorarioInicioEhDuranteAlgumAgendamentoDoBarbeiro(
+            agendamentosDoDia,
+            horarioInicio,
+            horariosDoBarbeiro
+        )
+    }
+
+    private fun filtraAgendamentosPorData(barbeiro: Barbeiro, horarioInicio: LocalDateTime): List<Agendamento> {
+        return barbeiro.agendamentos.filter { agendamento ->
+            agendamento.horario.toLocalDate() == horarioInicio.toLocalDate()
+        }
+    }
+
+    private fun getHorariosDoBarbeiro(
+        listaDeagendamentosDoDia: List<Agendamento>,
+        horarioDeInicioDosAgendamentos: List<LocalDateTime>,
+        horarioDeTerminoDosAgendamentos: List<LocalDateTime>
+    ): HashMap<UUID, Pair<LocalDateTime, LocalDateTime>> {
+
+        val agendamentosBarbeiro = HashMap<UUID, Pair<LocalDateTime, LocalDateTime>>()
+
+        listaDeagendamentosDoDia.forEachIndexed { index, _ ->
+            agendamentosBarbeiro[listaDeagendamentosDoDia[index].id!!] =
+                Pair(horarioDeInicioDosAgendamentos[index], horarioDeTerminoDosAgendamentos[index])
+        }
+
+        return agendamentosBarbeiro
+    }
+
+    private fun getHorarioDeInicioDeCadaAgendamento(agendamentosDoDia: List<Agendamento>): List<LocalDateTime> {
+        return agendamentosDoDia.map { agendamento ->
+            agendamento.horario
+        }
+    }
+
+    private fun getHorarioDeTerminoDeCadaAgendamento(agendamentosDoDia: List<Agendamento>): List<LocalDateTime> {
+        return agendamentosDoDia.map { agendamento ->
+            calcularHorarioDeTermino(agendamento.horario, agendamento.obterServicos())
+        }
+    }
+
+    private fun calcularHorarioDeTermino(horario: LocalDateTime, servicos: List<Servico>): LocalDateTime {
+        val duracaoTotal = servicos.map { servico ->
+            servico.duracacao
+        }.reduce { acc, i ->
+            acc + i
+        }
+
+        return horario.plusMinutes(duracaoTotal.toLong())
+    }
+
+    private fun verificaSeHorarioInicioEhDuranteAlgumAgendamentoDoBarbeiro(
+        agendamentosDoDia: List<Agendamento>,
+        horarioInicio: LocalDateTime,
+        horariosDoBarbeiro: HashMap<UUID, Pair<LocalDateTime, LocalDateTime>>
+    ): Boolean {
+        return agendamentosDoDia.any { agendamento ->
+            horarioInicio >= horariosDoBarbeiro[agendamento.id]!!.first &&
+                    horarioInicio <= horariosDoBarbeiro[agendamento.id]!!.second
+        }
     }
 
 }
