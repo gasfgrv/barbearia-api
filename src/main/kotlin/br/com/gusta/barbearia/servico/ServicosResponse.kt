9327c5e1497d650b2ec18a739035edede4e59d8f
Gustavo <gustavo_almeida11@hotmail.com>
2023-05-26 18:34:30 -0300

    Adição de links na listagem de serviços

--- src/main/kotlin/br/com/gusta/barbearia/servico/ServicosResponse.kt
+++ src/main/kotlin/br/com/gusta/barbearia/servico/ServicosResponse.kt
@@ -1,16 +1,24 @@
 package br.com.gusta.barbearia.servico
 
+import org.springframework.hateoas.RepresentationModel
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
+
 data class ServicosResponse(
     val nome: String,
     val descricao: String
-) {
+) : RepresentationModel<ServicosResponse>() {
 
     companion object Mapper {
         fun paraResposta(servico: Servico): ServicosResponse {
-            return ServicosResponse(
+            val resposta = ServicosResponse(
                 servico.nome,
                 servico.descricao
             )
+
+            resposta.add(linkTo(methodOn(ServicoController::class.java).detalharServico(servico.id!!)).withSelfRel())
+
+            return resposta
         }
     }
 
