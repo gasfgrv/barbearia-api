590ba850fbe0956ad082d94b4194919e548572be
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-06-06 23:02:09 -0300

    funções para encode e decode de strings em base64

--- src/main/kotlin/br/com/gusta/barbearia/utils/StringUtils.kt
+++ src/main/kotlin/br/com/gusta/barbearia/utils/StringUtils.kt
@@ -1,5 +1,6 @@
 package br.com.gusta.barbearia.utils
 
+import java.util.Base64
 import java.util.Objects
 import java.util.regex.Pattern
 
@@ -21,5 +22,17 @@ class StringUtils {
 
             return result.substring(0, 1).uppercase() + result.substring(1).lowercase();
         }
+
+        fun encodeParaBase64(string: String): String {
+            val encoder = Base64.getEncoder()
+            return encoder.encodeToString(string.toByteArray())
+        }
+
+        fun decodeDeBase64(string: String): String {
+            val decoder = Base64.getDecoder()
+            val stringByteArray = decoder.decode(string)
+            return stringByteArray.decodeToString()
+        }
+
     }
 }
